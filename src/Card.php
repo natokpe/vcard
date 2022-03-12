dff297aa22db1ef46ccad89a5be67aa1dcf02442
Nat Okpe <natokpe@gmail.com>
2022-03-12 15:00:38 +0100

    Added Card

---
+++ src/Card.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types = 1);
+
+namespace natokpe\vcard;
+
+/**
+ * A vCard entity
+ * @author Nat Okpe <natokpe@gmail.com>
+ */
+class Card
+{
+    /**
+     * 
+     */
+    public function __construct()
+    {}
+}
