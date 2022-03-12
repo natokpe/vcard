60271ecc87b902a3a339fe614d6d2c4c87569139
Nat Okpe <natokpe@gmail.com>
2022-03-12 14:59:56 +0100

    Added Parser

---
+++ src/Parser.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types = 1);
+
+namespace natokpe\vcard;
+
+/**
+ * @author Nat Okpe <natokpe@gmail.com>
+ */
+class Parser
+{
+    /**
+     * 
+     */
+    public function __construct()
+    {}
+}
